ad622a17515d4abe3944564b103e43df558a4826
nexusghost1225 <JGBxmas2000@hotmail.com>
2022-11-22 22:14:37 -0500

    updated js for api

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,19 @@
 $.ajax({
     url: "/api/getWeather",
     data: {
-        zipcode: 97201
+        zipcode: 25425
     },
     success: function (result) {
         $("#weather-temp").html("<strong>" + result + "</strong> degrees");
     }
-});
+})
 
-api.openweathermap.org/data/2.5/forecast?q=Harper Ferry,US&appid=9a53d50290abc4a394e976ac7ee518f7
+get = api.openweathermap.org / data / 2.5 / forecast ? q = Harper Ferry, US & appid=9a53d50290abc4a394e976ac7ee518f7
+
+var status = "api.openweathermap.org/data/2.5 / forecast ? lat = 44.34 & lon=10.99 & appid=9a53d50290abc4a394e976ac7ee518f7";
+
+
+for (let i = 0; i < status; +i +) {
+    [const element = status]i;
+
+}
